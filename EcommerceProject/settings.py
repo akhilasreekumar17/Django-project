1a63edb8b94f2c5d80e2a5cea212f8aa541d8116
Akhil Sreekumar <akhilasreekumar17@gmail.com>
2025-12-24 16:19:12 +0530

    host

--- EcommerceProject/settings.py
+++ EcommerceProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(tpczpx9e36+=udy*+rgsr7(4ix@r&%(vqspyxjsy0wm!jx&qm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['akhilasreekumar.pythonanywhere.com']
 
 
 # Application definition
