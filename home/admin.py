8727cfac72958c27cf709323a190671b095293be
Akhil Sreekumar <akhilasreekumar17@gmail.com>
2025-12-24 15:17:36 +0530

    Fix admin.py: remove invalid User registration

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import*
-
-# Register your models here.
+from .models import Product, Order, OrderItem
 
 admin.site.register(Product)
 admin.site.register(Order)
-admin.site.register(OrderProduct)
+admin.site.register(OrderItem)
